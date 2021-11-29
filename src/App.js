73e6932f9bd6d2a1aaa3cfba3e464c506e11cc8d
kmwangemi <mwangemik@gmail.com>
2021-11-29 23:41:46 +0300

    add multiple reducers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Provider } from 'react-redux';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
 import store from './redux/store';
 
 function App() {
    return (
       <Provider store={store}>
          <div className="App">
+            <HooksCakeContainer/>
             <CakeContainer />
+            <IceCreamContainer/>
          </div>
       </Provider>
    );
